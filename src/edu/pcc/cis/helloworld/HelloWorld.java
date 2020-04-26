89714baf0248125ed5fb5e6787c57ba1b9a94254
Nicole Keefer <nicolemk1124@gmail.com>
2020-04-26 16:06:00 -0700

    Added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Author: Nicole Keefer
+ * Version: 2020.04.26
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
